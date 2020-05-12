826cb70ac8859fd65f95c8143a58bfcda4dce63b
Jonathan Mwizerwa <jmwizerw@ggc.edu>
2020-05-12 03:42:51 -0400

    some more changes

--- resources.tf
+++ resources.tf
@@ -5,7 +5,7 @@ provider "aws" {
 
 resource "aws_key_pair" "demo_key" {
   key_name   = "MyKeyPair"
-  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCvgGkgXMOR0GTYrXuBq1CYXCGVSJLY5Z4ZyRyQMVEWMTWzgvJ6gm5aZLQICGZ+X9DMWZFF602vbjNBzGtzV1J6ce7UCGpz7RbnuPTEuINo7WqoUm2FHmodT8fazwUqGvkse10HCylBBHI+7bX8c4+pYoYi+j9d5d3Cgut2KZrrEHgZvbv48SAyOGPDMo5w+aPFA3D6LxJKBGVWlVH+52EjS0LLNOO/7ATGcjv35jnrgDndK50ZXK3RdS3Z9Fy1Z+lj3+Bi/wscAlodrl6Z3WH3r6NfX/MNLopViPFj6jTbHONyF4anVrSOtqAKyT6udRCKpRqdqz71HeU0O+XXEum5 trust@MacAroni.local"
+  public_key = var.public_key
 }
 
 resource "aws_instance" "jenkins" {
